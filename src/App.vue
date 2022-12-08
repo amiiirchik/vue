ee1430340055227470eccfc10549f0b5d13bd196
Alex <roden20030823@mail.ru>
2022-12-08 17:45:59 +0200

    fixes are necessary

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <HelloWorld msg="test" />
+</template>
+
+<style scoped lang="sass">
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { computed, reactive } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+let elems = reactive([1, 2, 3, 4, 5, 6])
+
+computed(function filtered(elems){
+  return this.elems.filter(i => i % 2 === 0)
+}) 
+
+</script>
+
+<template>
+<div class="container pt-5" id="app2">
+  <div class="card center">
+    <input @keyup.enter="addItem($event)">
+    <button class="btn" @click="msg='Новое значение'">Добавить</button>
+    <ul class="list" v-if="elems.length">
+      <li class="list-item"
+       @click="elems.splice(index, 1)"
+       v-for="(elem, index) in filtered"
+       :key = elem
+       >
+       index{{index}} - {{elem}}
+       
+        <input @click.stop>
+      </li>
+    </ul>
+    <p v-else>Массив равен нулю</p>
+  </div>
+</div>
+
+</template>
+
+<style scoped lang="sass">
+
+</style>
