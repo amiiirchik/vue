5e4ba2315d7fae16bc9b49741a3201e6859be1ad
amiiirchik <ragimovamir@yandex.ru>
2022-12-11 15:50:37 +0300

    final + can be updated

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,36 +1,46 @@
 <script setup>
-import { computed, reactive } from 'vue'
+import { computed, reactive, ref } from 'vue'
 
-let elems = reactive([1, 2, 3, 4, 5, 6])
 
-computed(function filtered(elems){
-  return this.elems.filter(i => i % 2 === 0)
-}) 
+let elems = reactive(['Попить чай', 'Покодить', 'Написать диплом'])
+
+  function filtered(){
+    return elems
+    //.filter(i => i % 2 === 0)
+  }
+
+  function addItem(inp){
+    let input = ref(inp)
+    elems.push(input._value)
+  }
 
 </script>
 
 <template>
 <div class="container pt-5" id="app2">
-  <div class="card center">
-    <input @keyup.enter="addItem($event)">
-    <button class="btn" @click="msg='Новое значение'">Добавить</button>
+  <div class="card center form-control">
+    <input v-model="inp" @keyup.enter="addItem(inp)">
+    <button class="btn" @click="addItem(inp)">Добавить</button>
     <ul class="list" v-if="elems.length">
       <li class="list-item"
        @click="elems.splice(index, 1)"
-       v-for="(elem, index) in elems"
+       v-for="(elem, index) in filtered()"
        :key = elem
        >
-       index{{index}} - {{elem}}
+       {{index}}
        
-        <input @click.stop>
+        <input id="itemInput" @click.stop :value="elem">
       </li>
     </ul>
-    <p v-else>Массив равен нулю</p>
+    <p v-else>У вас нет текущих дел.</p>
   </div>
 </div>
 
 </template>
 
 <style scoped lang="sass">
+  #itemInput
+    margin-left: 10px
 
+  
 </style>
